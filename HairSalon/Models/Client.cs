3fcaf9c73f6371be98e651e832d6ed5fc1360782
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-03-15 15:05:29 -0700

    deny client link if stylist list == 0

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -1,10 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace HairSalon.Models
 {
   public class Client
   {
     public int ClientId { get; set; }
     public string Name { get; set; }
+
     public int StylistId { get; set; }
+
     public virtual Stylist Stylist { get; set; }
   }
 }
